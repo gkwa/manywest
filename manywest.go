e2434bc2dd9008bd9943d38cd8acc8afd5262805
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-19 20:18:20 -0800

    we can autogenerate make_txtar.sh now

--- manywest.go
+++ manywest.go
@@ -2,7 +2,12 @@ package manywest
 
 import (
 	"flag"
+	"fmt"
 	"log/slog"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
 )
 
 type Options struct {
@@ -40,11 +45,129 @@ func parseArgs() Options {
 	return options
 }
 
-func run(options Options) error {
-	slog.Debug("test", "test", "Debug")
-	slog.Debug("test", "LogLevel", options.LogLevel)
-	slog.Info("test", "test", "Info")
-	slog.Error("test", "test", "Error")
+const templateScript = `#!/usr/bin/env bash
 
+# set -e
+
+tmp=$(mktemp -d {{.Cwd}}.XXXXX)
+
+if [ -z "${tmp+x}" ] || [ -z "$tmp" ]; then
+    slog.Error("Error: $tmp is not set or is an empty string.")
+    exit 1
+fi
+
+{
+    rg --files . \
+		| grep -vE $tmp/filelist.txt \
+		| grep -vE make_txtar.sh \
+        {{range .Files}}| grep -vE {{.}} \
+        {{end}}
+} | tee $tmp/filelist.txt
+
+tar -cf $tmp/{{.Cwd}}.tar -T $tmp/filelist.txt
+mkdir -p $tmp/{{.Cwd}}
+tar xf $tmp/{{.Cwd}}.tar -C $tmp/{{.Cwd}}
+rg --files $tmp/{{.Cwd}}
+txtar-c $tmp/{{.Cwd}} | pbcopy
+
+rm -rf $tmp
+`
+
+func run(options Options) error {
+	filename := "make_txtar.sh"
+	_, err := os.Stat(filename)
+	if err == nil {
+		slog.Info("file exists, quiting early to prevent overwriting", "file", filename)
+		return nil
+	}
+
+	fileList, err := recurseDirectory(".")
+	if err != nil {
+		slog.Error("Error:", "error", err)
+		return err
+	}
+
+	if len(fileList) > 20 {
+		slog.Error("Error: Number of files is greater than 20.", "fileCount", len(fileList))
+		return err
+	}
+
+	tmpl, err := template.New("script").Parse(templateScript)
+	if err != nil {
+		slog.Error("Error:", "error", err)
+		return err
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		slog.Error("Error:", "error", err)
+		return err
+	}
+
+	// convert 'manywest' to regex 'manywest$'
+	cwdName := filepath.Base(cwd)
+	matchString := cwdName
+	replaceString := fmt.Sprintf("%s$", cwdName)
+	for i, s := range fileList {
+		if s == matchString {
+			fileList[i] = replaceString
+		}
+	}
+
+	data := struct {
+		Files []string
+		Cwd   string
+	}{
+		Files: fileList,
+		Cwd:   cwdName,
+	}
+
+	var scriptBuilder strings.Builder
+	err = tmpl.Execute(&scriptBuilder, data)
+	if err != nil {
+		slog.Error("Error:", "error", err)
+		return err
+	}
+
+	scriptFileName := "make_txtar.sh"
+	err = os.WriteFile(scriptFileName, []byte(scriptBuilder.String()), 0o755)
+	if err != nil {
+		slog.Error("Error:", "error", err)
+		return err
+	}
+
+	slog.Debug("Script created successfully", "script", scriptFileName)
 	return nil
+}
+
+func recurseDirectory(root string) ([]string, error) {
+	var fileList []string
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() && info.Name() == ".git" {
+			return filepath.SkipDir
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if isExcludedFile(info.Name()) {
+			return nil
+		}
+
+		fileList = append(fileList, path)
+		return nil
+	})
+
+	return fileList, err
+}
+
+func isExcludedFile(fileName string) bool {
+	excludedFiles := map[string]bool{
+		// Add more excluded files as needed
+	}
+
+	return excludedFiles[fileName]
 }
